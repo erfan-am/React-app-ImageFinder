0df8e43a743c1c0e401cfaba3c86950f29a5eeb1
erfan-am <erfanam582@gmail.com>
2020-04-30 02:13:42 -0700

    Updates

---
+++ precache-manifest.a50a13d1fd17a4d309a274eafe215acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c778fbfe65f1ed7e6ca1f07a864f939",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5c671eca942969fda11f",
+    "url": "/react-app/static/css/main.e430fa06.chunk.css"
+  },
+  {
+    "revision": "d34d371f331349d61a44",
+    "url": "/react-app/static/js/2.7f7cf36c.chunk.js"
+  },
+  {
+    "revision": "87a31a2160d6241b73aedb52f881f9cc",
+    "url": "/react-app/static/js/2.7f7cf36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c671eca942969fda11f",
+    "url": "/react-app/static/js/main.c2ae7b7e.chunk.js"
+  },
+  {
+    "revision": "1bd4be4e3398d6925a8e",
+    "url": "/react-app/static/js/runtime-main.b487676f.js"
+  }
+]);
